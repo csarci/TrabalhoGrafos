5e0543c4fa6d960585245f25985bb7579001272e
csarci <csarci@sti.uff.br>
2016-02-02 17:45:45 -0200

    Update Vertice.java

--- src/trabalhoGrafos/Vertice.java
+++ src/trabalhoGrafos/Vertice.java
@@ -1,47 +1,67 @@
-package grafo;
+	import java.util.ArrayList;
+	import java.util.List;
 
-import java.util.ArrayList;
-import java.util.List;
+	/**
+	 *
+	 * @author tayanemoura e carlossarcinelli
+	 */
+	class Vertice {
 
-/**
- *
- * @author tayanemoura e carlossarcinelli
- */
-class Vertice {
+	    int indice;
+	    private int cor;
+	    List<Vertice> vizinhos;
+	    private int PE;
+	    private int PS;
+	    private Vertice pai;
+	    private int back;
 
-    int indice;
-    List<Vertice> vizinhos;
-    private int PE;
-    private int PS;
-    private Vertice pai;
+	    Vertice(int indice) {
+	        this.indice = indice;
+	        this.vizinhos = new ArrayList<>();
+	    }
 
-    Vertice(int indice) {
-        this.indice = indice;
-        this.vizinhos = new ArrayList<>();
-    }
+	    void setPai(Vertice v){
+	        this.pai = v;
+	    }
+	    
+	    void setPE (int n){
+	        this.PE = n;
+	    }
+	    
+	    void setPS(int n){
+	        this.PS = n;
+	    }
+	    
+	    Vertice getPai(){
+	        return this.pai;
+	    }
+	    
+	    int getPE (){
+	        return this.PE;
+	    }
+	    
+	    int getPS(){
+	        return this.PS;
+	    }
 
-    void setPai(Vertice v){
-        this.pai = v;
-    }
-    
-    void setPE (int n){
-        this.PE = n;
-    }
-    
-    void setPS(int n){
-        this.PS = n;
-    }
-    
-    Vertice getPai(){
-        return this.pai;
-    }
-    
-    int getPE (){
-        return this.PE;
-    }
-    
-    int getPS(){
-        return this.PS;
-    }
-    
-}
+		public void setCor(int cor) {
+			this.cor = cor;
+		}
+
+		public int getCor() {
+			return this.cor;
+		}
+
+		public void setBack(int back) {
+			this.back = back;
+		}
+
+		public int getBack() {
+			return this.back;
+		}
+	    
+		public String toString(){
+			return String.valueOf(this.indice);
+		}
+	}
+
