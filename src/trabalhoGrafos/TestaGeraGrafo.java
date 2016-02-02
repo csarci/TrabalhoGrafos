4a98b54f8fcaaf1ef60f1316d25cfeed45d03d54
csarci <csarci@sti.uff.br>
2016-02-02 17:45:57 -0200

    Update TestaGeraGrafo.java

--- src/trabalhoGrafos/TestaGeraGrafo.java
+++ src/trabalhoGrafos/TestaGeraGrafo.java
@@ -1,22 +1,13 @@
-package grafo;
+	public class TestaGeraGrafo {
 
-public class TestaGeraGrafo {
-
-	public static void main(String[] args) {
-		Grafo g = new Grafo();
-		g.geraGrafo(7, 0.5f);
-		System.out.println(g.toString());
-		
-		BuscaProfundidade dps1 = new BuscaProfundidade(g);
-		
-		dps1.inicializaBusca();
-		dps1.verificaConexo();
-		dps1.verificaArvore();
-		
-		g.buscaCompConexas();
-		g.buscaPontes();
-		g.buscaArticulacoes();
-		g.buscaBlocos();
+		public static void main(String[] args) {
+			Grafo g = new Grafo();
+			g.geraGrafo(5, 0.5f);
+			System.out.println(g.toString());
+			
+			BuscaProfundidade dps = new BuscaProfundidade(g);
+			dps.resetaValores();
+			dps.realizaBusca(g.vertices.get(0));
+		}
 	}
 
-}
