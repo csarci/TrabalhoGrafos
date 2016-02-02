d9b300a7e7a2fd638fdd63309d2a2174746c4f45
csarci <csarci@sti.uff.br>
2016-02-02 05:11:00 -0200

    Update Vertice.java

--- src/trabalhoGrafos/Vertice.java
+++ src/trabalhoGrafos/Vertice.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package trabalhoGrafos;
+package grafo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +9,39 @@ import java.util.List;
  */
 class Vertice {
 
-    String nome;
-    //lista dos vértices adjacentes
-    List<Vertice> adj;
+    int indice;
+    List<Vertice> vizinhos;
+    private int PE;
+    private int PS;
+    private Vertice pai;
 
-    Vertice(String nome) {
-        this.nome = nome;
-        this.adj = new ArrayList<>();
+    Vertice(int indice) {
+        this.indice = indice;
+        this.vizinhos = new ArrayList<>();
     }
 
-    void addAdj(Vertice v) {
-        adj.add(v);
+    void setPai(Vertice v){
+        this.pai = v;
     }
+    
+    void setPE (int n){
+        this.PE = n;
+    }
+    
+    void setPS(int n){
+        this.PS = n;
+    }
+    
+    Vertice getPai(){
+        return this.pai;
+    }
+    
+    int getPE (){
+        return this.PE;
+    }
+    
+    int getPS(){
+        return this.PS;
+    }
+    
 }
